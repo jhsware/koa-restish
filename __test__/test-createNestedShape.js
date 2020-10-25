83683c1150912c0626f2b90241382a8abb195d98
jhsware <sebastian@urbantalk.se>
2020-10-25 16:26:25 +0100

    Create nested shape objects

---
+++ __test__/test-createNestedShape.js
@@ -0,0 +1,81 @@
+require("@babel/polyfill")
+const { expect } = require('chai')
+const { createNestedShape } = require('../lib/purgeWithShape')
+
+describe("create nested shape", () => {
+
+  describe("return entire object", () => {
+    it("Anything", async function () {
+      const tmp = createNestedShape()
+      expect(tmp).to.equal(undefined)
+    })
+    it("First level", async function () {
+      const tmp = createNestedShape('*')
+      const outp = [['*']]
+      expect(JSON.stringify(tmp)).to.equal(JSON.stringify(outp))
+    })
+    it("Three levels", async function () {
+      const tmp = createNestedShape('*.*.*')
+      const outp = [['*'], ['*.*'], ['*.*.*']]
+      expect(JSON.stringify(tmp)).to.equal(JSON.stringify(outp))
+    })
+    it("All primites of order", async function () {
+      const tmp = createNestedShape([
+        '*',
+        'order.*'
+      ])
+      const outp = [
+        ['*'],
+        ['order.*']
+      ]
+      expect(JSON.stringify(tmp)).to.equal(JSON.stringify(outp))
+    })
+    it("More specific translation of order with omit", async function () {
+      const tmp = createNestedShape([
+        "*",
+        "order.*.*",
+        "order.!orderRows[]",
+      ])
+      const outp = [
+        ['*'],
+        ['order.*', 'order.!orderRows[]'],
+        ['order.*.*']
+      ]
+      expect(JSON.stringify(tmp)).to.equal(JSON.stringify(outp))
+    })
+    /*
+    it("More specific translation of order with array options", async function () {
+      const tmp = createNestedShape([
+        "*",
+        "order.*.*",
+        "order.orderRows[0-9]",
+      ])
+      const outp = [
+        ['*'],
+        ['order.*', 'order.orderRows[0-9]'],
+        ['order.*.*']
+      ]
+      expect(JSON.stringify(tmp)).to.equal(JSON.stringify(outp))
+    })
+    */
+    it("Very specific translation of order", async function () {
+      const tmp = createNestedShape([
+        "discountCode",
+        "firstRow",
+        "order",
+        "order.orderDate",
+        "order.orderCurrency",
+        "order.orderRows[]",
+        "order.orderRows[].itemDescription",
+        "order.orderRows[].sku",
+        "order.orderRows[].qty",
+      ])
+      const outp = [
+        ['discountCode', 'firstRow', 'order'],
+        ['order.orderDate', 'order.orderCurrency', 'order.orderRows[]'],
+        ['order.orderRows[].itemDescription', 'order.orderRows[].sku', 'order.orderRows[].qty']
+      ]
+      expect(JSON.stringify(tmp)).to.equal(JSON.stringify(outp))
+    })
+  })  
+})
